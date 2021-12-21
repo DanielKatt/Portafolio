f6106a69070808216b97145d3219ecfb774c8eb6
DanielKatt <89039660+DanielKatt@users.noreply.github.com>
2021-12-20 22:12:18 -0500

    Adding menu and trying to correct navigation menu with javascriot. STILL DOESNT WORK AAAAAAAAAAAAAAAA

---
+++ Javascript/menu.js
@@ -0,0 +1,6 @@
+const hamburguer = document.querySelector('.hamburguer')
+const menu = document.querySelector('.menu-navegacion')
+
+hamburguer.addEventListener('click', () =>{
+    menu.classList.toggle("spread")
+})
